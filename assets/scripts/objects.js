860d1b0fec3077c53c0c1a448cdd4fbd1852f15b
Prithvi <prithvirajkumar14@gmail.com>
2020-06-12 19:12:49 -0400

    Initial commit

---
+++ assets/scripts/objects.js
@@ -0,0 +1,59 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  const filterMovies = !filter ? movies : movies.filter((movie) => {
+    movie.info.title.includes(filter);
+  });
+
+  filterMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+    return;
+  }
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+  movies.push(newMovie);
+  renderMovies()
+};
+
+const searchMovieHandler = () => {
+  const filterTerm = document.getElementById('filter-term').value;
+  renderMovies(filterTerm);
+}
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
